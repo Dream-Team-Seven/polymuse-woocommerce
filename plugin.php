8ce57fdd8b0ff01a17904f560075c71068a6dfb3
Pmacdon15 <pmacdonald15@gmail.com>
2025-03-06 16:40:20 -0700

    revert

--- plugin.php
+++ plugin.php
@@ -8,6 +8,7 @@ Author: Corazon Palencia, Gary Simwawa, Patrick MacDonald, Xiangyu Hou, Tim Kara
 Author URI: https://polymuse.tech/
 */
 
+
 if (!defined('ABSPATH')) {
     exit; // Exit if accessed directly
 }
@@ -16,7 +17,8 @@ if (!defined('ABSPATH')) {
 if (in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_option('active_plugins')))) {
 
     // Add theme warning
-    function my_polymuse_theme_warning_plugin_init() {
+    function my_polymuse_theme_warning_plugin_init()
+    {
         $current_theme = get_template();
         $suboptimal_themes = array(
             'oceanwp',
@@ -27,7 +29,8 @@ if (in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_
         }
     }
 
-    function my_polymuse_plugin_styling_warning() {
+    function my_polymuse_plugin_styling_warning()
+    {
         ?>
         <div class="is-dismissible notice notice-warning">
             <p>
@@ -42,7 +45,8 @@ if (in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_
     add_action('plugins_loaded', 'my_polymuse_theme_warning_plugin_init');
 
     // Add custom field to product editor
-    function polymuse_custom_field() {
+    function polymuse_custom_field()
+    {
         woocommerce_wp_text_input(
             array(
                 'id' => '_3d_model_url',
@@ -55,7 +59,8 @@ if (in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_
     add_action('woocommerce_product_options_general_product_data', 'polymuse_custom_field');
 
     // Save custom field data
-    function polymuse_save_custom_field($post_id) {
+    function polymuse_save_custom_field($post_id)
+    {
         $model_url = $_POST['_3d_model_url'];
         if (!empty($model_url)) {
             update_post_meta($post_id, '_3d_model_url', esc_url($model_url));
@@ -64,7 +69,9 @@ if (in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_
     add_action('woocommerce_process_product_meta', 'polymuse_save_custom_field');
 
     // Add 3D model to product gallery
-    function polymuse_add_model_and_thumbnail_to_gallery($html, $attachment_id) {
+
+    function polymuse_add_model_and_thumbnail_to_gallery($html, $attachment_id)
+    {
         global $product;
 
         // Debug logging
@@ -95,12 +102,14 @@ if (in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_
                 $model_viewer .= 'data-thumb-alt="3D Model" ';
                 $model_viewer .= 'data-thumb-srcset="' . esc_url($model_thumbnail_url) . ' 100w" ';
                 $model_viewer .= 'data-thumb-sizes="(max-width: 100px) 100vw, 100px" ';
-                $model_viewer .= 'class="woocommerce-product-gallery__image polymuse-model-viewer no-swipe" '; // Changed: Added no-swipe class
-                $model_viewer .= 'style="position: relative; overflow: hidden;">'; // Changed: Added inline styles
+                $model_viewer .= 'class="woocommerce-product-gallery__image polymuse-model-viewer" ">';
                 $model_viewer .= '<model-viewer src="' . esc_url($model_url) . '" alt="3D model of ' . esc_attr($product->get_name()) . '" auto-rotate camera-controls ar ar-modes="webxr scene-viewer quick-look" style="width: 100%; height: 100%;"></model-viewer>';
                 $model_viewer .= '</div>';
 
-                // Hide default placeholder
+                // Hide default this will make selecting variants work properly
+                // with out this when you select a variant product  there will be a place holder image out of place
+                // The down side is then the main product image will not show up in the carousel or thumb nail
+                // A benefit is that when you select a variant the main image will not change and show the model viewer
                 $html = '<style>.woocommerce-product-gallery__image--placeholder:first-child { display: none; }</style>';
                 error_log('Modified HTML: ' . $html);
                 return $model_viewer . $html;
@@ -111,94 +120,33 @@ if (in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_
     }
     add_filter('woocommerce_single_product_image_thumbnail_html', 'polymuse_add_model_and_thumbnail_to_gallery', 10, 4);
 
-    function add_buttons_container() {
+    function add_buttons_container()
+    {
         global $product;
 
         if (is_product()) {
+
+            // Create a container div for variant options
             ?>
             <div id="variant-options-container"></div>
             <?php
         }
     }
     add_action('woocommerce_before_add_to_cart_form', 'add_buttons_container');
-
+    
     // Add model-viewer script to header
-    function polymuse_add_model_viewer_script() {
+    function polymuse_add_model_viewer_script()
+    {
         echo '<script type="module" src="https://unpkg.com/@google/model-viewer/dist/model-viewer.min.js"></script>';
     }
     add_action('wp_head', 'polymuse_add_model_viewer_script');
 
     // Enqueue styles and scripts
-    function polymuse_enqueue_assets() {
+    function polymuse_enqueue_assets()
+    {
         wp_enqueue_style('polymuse-styles', plugins_url('styles.css', __FILE__));
         wp_enqueue_script('polymuse-script', plugins_url('polymuse.js', __FILE__), array('jquery'), '1.0', true);
     }
-    add_action('wp_enqueue_scripts', 'polymuse_enqueue_assets', 20); // Changed: Increased priority
+    add_action('wp_enqueue_scripts', 'polymuse_enqueue_assets');
 
-    // New: Disable WooCommerce gallery slider functionality
-    add_action('wp', 'polymuse_disable_gallery_slider', 100);
-    function polymuse_disable_gallery_slider() {
-        if (is_product()) {
-            // Remove default slider initialization
-            // wp_dequeue_script('flexslider');
-            
-            // // Remove WooCommerce gallery classes that trigger sliding
-            add_filter('woocommerce_single_product_image_gallery_classes', 'polymuse_remove_slider_classes');
-            
-            // Add custom CSS to prevent swiping
-            add_action('wp_enqueue_scripts', 'polymuse_disable_swipe_css');
-        }
-    }
-
-    // New: Remove slider classes
-    function polymuse_remove_slider_classes($classes) {
-        $classes = array_diff($classes, array(
-            'woocommerce-product-gallery--with-images',
-            'woocommerce-product-gallery--columns-4',
-            'images'
-        ));
-        $classes[] = 'polymuse-no-slider';
-        return $classes;
-    }
-
-    // New: Add CSS to disable swiping
-    function polymuse_disable_swipe_css() {
-        wp_enqueue_style('polymuse-styles', plugins_url('styles.css', __FILE__));
-        $custom_css = "
-            .woocommerce-product-gallery {
-                opacity: 1 !important;
-            }
-            .woocommerce-product-gallery__wrapper {
-                transform: none !important;
-                transition: none !important;
-                overflow: hidden !important;
-            }
-            .polymuse-no-slider .woocommerce-product-gallery__image {
-                float: none !important;
-                width: 100% !important;
-                margin: 0 !important;
-            }
-            .polymuse-model-viewer {
-                touch-action: pan-y !important; /* Allows vertical scroll but prevents horizontal swipe */
-                -webkit-user-select: none !important;
-                user-select: none !important;
-            }
-            .no-swipe {
-                -webkit-overflow-scrolling: auto !important;
-                overscroll-behavior-x: none !important;
-            }
-            
-            /* Mobile-specific rules */
-            @media (max-width: 768px) {
-                .woocommerce-product-gallery__wrapper {
-                    display: block !important;
-                }
-                .polymuse-model-viewer {
-                    width: 100% !important;
-                    height: 400px !important;
-                }
-            }
-        ";
-        wp_add_inline_style('polymuse-styles', $custom_css);
-    }
 }
