e208fabf3ba9c8398972182f871e729597f5dc07
Pmacdon15 <pmacdonald15@gmail.com>
2025-03-06 16:48:56 -0700

    trying to new hook

--- plugin.php
+++ plugin.php
@@ -133,7 +133,7 @@ if (in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_
         }
     }
     add_action('woocommerce_before_add_to_cart_form', 'add_buttons_container');
-    
+
     // Add model-viewer script to header
     function polymuse_add_model_viewer_script()
     {
@@ -150,5 +150,12 @@ if (in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_
     add_action('wp_enqueue_scripts', 'polymuse_enqueue_assets');
 
     // Block swiping on model viewer
-    // remove_theme_support('wc-product-gallery-swipe');
+    function disable_swipe_in_gallery_on_mobile()
+    {
+        if (wp_is_mobile()) {
+            wp_enqueue_script('disable-swipe', plugins_url('disable-swipe.js', __FILE__), array('jQuery'), '1.0', true);
+        }
+    }
+
+    add_action('wp_enqueue_scripts', 'disable_swipe_in_gallery_on_mobile');
 }
